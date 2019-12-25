b6b937958d5ecf8d03f6edd00ee29d171275cf94
m-walid <49622775+m-walid@users.noreply.github.com>
2019-12-25 05:37:35 +0200

    Revert "fixed xml conflict"
    This reverts commit def0da3aeeba682dd8b442cfea26927749588216, reversing
    changes made to d8cdef48b21d4e80ca28f54c1181681d7c964175.

--- src/Core/LoginForm.java
+++ src/Core/LoginForm.java
@@ -1,62 +1,34 @@
 package Core;
 
-import UI.Login_Scene;
-import com.jfoenix.controls.JFXButton;
-import javafx.fxml.FXML;
-import javafx.scene.Scene;
-
-import java.util.ArrayList;
-
-
 public class LoginForm extends Form {
-    public static int attempts;
+    private int attempts;
     private boolean validation;
     private boolean attemptsValidation;
-    //private ArrayList<User> myUsers = DatabaseManager.getUser();// should have list of users from database
+    // should have list of users from database
 
-    public LoginForm(String username,String password) {
-        setUserName(username);
-        setPassword(password);
-
+    public LoginForm() {
       validation=checkCredentials();  // check validation
         if (validation){             // name and pass is right
             // go to new screen
-            Scene scene=new Scene(Login_Scene.Main_Menu);
-            Login_Scene.primaryStage.setScene(scene);
-            Login_Scene.primaryStage.show();
         }else {                     // name and pass is wrong
             attempts++;
-            System.out.println(attempts);
            attemptsValidation=checkAttempt();
             if (attemptsValidation){
                 //can try login again
             }else {
                 // can not try login again
-                if (attempts==3){
-                    System.out.println("you can not login again close NOW");
-
-
-                }
             }
         }
     }
 
     boolean checkCredentials(){
-        for (int i = 0; i <DatabaseManager.getUser().size() ; i++) {
-            System.out.println(DatabaseManager.getUser().get(i).getUsername());
-            System.out.println(DatabaseManager.getUser().get(i).getPassword());
-            System.out.println("-------------");
-            System.out.println(getUserName());
-            System.out.println(getPassword());
-            System.out.println("Checking");
-            if (DatabaseManager.getUser().get(i).getUsername().equals(getUserName()) &&
-                    DatabaseManager.getUser().get(i).getPassword().equals(getPassword())){          //if name and pass equal on of users list
-                System.out.println("Checked");
-                return true;
-            }
+        String name =getUserName();
+        String pass = getPassword();
+        if (true){  //if name and pass equal on of users list
+         return true;
+        }else{    //if their wrong
+            return false;
         }
-        //if name and pass wrong
-        return false;
     }
 
     Boolean checkAttempt(){ // check number of attempts
--- src/Core/User.java
+++ src/Core/User.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public abstract class User {
 
-    private static int id;
+    private int id;
     private String username;
     private String password;
     private String name;
@@ -12,53 +12,6 @@ public abstract class User {
     private String address;
     private String phoneNumber;
 
-    public static int getId() {
-        return id;
-    }
-
-    public static void setId(int id) {
-        User.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
     public ArrayList executeSearch(SearchManager search){
 
@@ -70,7 +23,6 @@ public abstract class User {
 
     public User(int id, String username, String password, String name, String email, String address, String phoneNumber) {
         this.id = id;
-        this.id++;
         this.username = username;
         this.password = password;
         this.name = name;
