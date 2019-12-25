bd08da96ab0df0282844ca1a3eee847351aaa615
m-walid <49622775+m-walid@users.noreply.github.com>
2019-12-25 15:09:29 +0200

    added methods in dbm

--- src/Core/DatabaseManager.java
+++ src/Core/DatabaseManager.java
@@ -6,6 +6,8 @@ public class DatabaseManager {
 
    private ArrayList<Donor>donors=new ArrayList<Donor>();
    private ArrayList<Charity>charities=new ArrayList<Charity>();
+   private ArrayList<Donation> donations=new ArrayList<Donation>();
+
 
     private static DatabaseManager instance =null;
 
@@ -41,6 +43,52 @@ public class DatabaseManager {
     public void insertCharity(Charity charity) {
         charities.add(charity);
     }
+
+
+    public Donor queryDonor(String username){
+        for ( Donor donor: donors) {
+            if(donor.getUsername().equals(username)) return donor;
+
+        }
+        return null;
+    }
+
+    public Charity queryCharity(String name){
+        for (Charity charity: charities) {
+            if (charity.getName().equals(name)) return charity;
+
+        }
+        return null;
+    }
+
+
+    public void deleteDonor(String username){
+        Donor donor=queryDonor(username);
+        donors.remove(donor);
+    }
+
+    public void deleteCharity(String name){
+        Charity charity =queryCharity(name);
+        charities.remove(charity);
+    }
+
+
+    public void insertDonation(Donation donation){
+        donations.add(donation);
+    }
+
+    public ArrayList<Donation> queryDonorDonations(Donor donor){
+        ArrayList<Donation> donations =new ArrayList<>();
+        for (Donation donation: this.donations) {
+            if(donation.getDonorId()==donor.getId()) donations.add(donation);
+
+        }
+        return donations;
+    }
+
+
+
+
 
 
 
